275db0f4a2d550f852be28a89038a2676efcfbc8
romeelkhan <romeel_k@yahoo.co.uk>
2015-10-24 15:27:18 +0100

    removed comment from api.js

--- api/api.js
+++ api/api.js
@@ -10,22 +10,6 @@ app.get('/', function (req, res) {
 })
 
 
-/*app.get('/api/posts', function (req, res, next) {
-  Post.find(function(err, posts) {
-    if (err) { return next(err) }
-    res.json(posts)
-  })
-})
-app.post('/api/posts', function (req, res, next) {
-  var post = new Post({
-    username: req.body.username,
-    body: req.body.body
-  })
-  post.save(function (err, post) {
-    if (err) { return next(err) }
-    res.json(201, post)
-  })
-})*/
 app.use(require('./controllers/api/posts'))
 app.use( require('./controllers/static'))
 app.use(function(req, res) {
